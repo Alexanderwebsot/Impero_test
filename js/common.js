b399eddc4e1bab15767ff9a2ba011fa44fb49075
Alexanderwebsot <64553081+Alexanderwebsot@users.noreply.github.com>
2020-09-19 07:02:17 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,29 +1,18 @@
 $(document).ready(function () {
-  $(".modal-phone").mask("+7 (999) 999-9999");
-  $('.header-slider').slick({
-    arrows: false,
-    dots: true,
-  })
-  $('.alert-close').on('click', function() {
-    let window_dark = $('.dark-window');
-    $(window_dark).addClass('dark-close');
-  })
-  $('.factories-slider').slick({
-    slidesToShow: 1,
-    prevArrow: $('.factories-arrow__left'),
-    nextArrow: $('.factories-arrow__right'),
-  })
-  $('.nav-select').selectize({
-      sortField: 'text'
-  });
-  $(".product-slider").slick({
-    prevArrow: $('.factories-arrow__left'),
-    nextArrow: $('.factories-arrow__right'),
-  })
-  $(".product-navslider").slick({
-    asNavFor: '.product-slider',
-    vertical: true,
-    slidesToShow: 4,
-    focusOnSelect: true
-  })
+ $('.menu-button-mobile-d').on('click', function() {
+ 	let menu = $('.nav-top-header');
+ 	$(menu).addClass('nav-top-header__active');
+ })
+ $('.menu-close__header').on('click', function() {
+ 	let menu = $('.nav-top-header');
+ 	$(menu).removeClass('nav-top-header__active');
+ })
+ $('.menu-button-mobile_2').on('click', function() {
+ 	let menu = $('.nav-bottom');
+ 	$(menu).addClass('nav-top-header__active_2');
+ })
+ $('.close-menu_b').on('click', function() {
+ 	let menu = $('.nav-bottom');
+ 	$(menu).removeClass('nav-top-header__active_2');
+ })
 });
